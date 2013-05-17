09339091882f9bd524263fd6e7d1b2e5ec2f15fa
Arthur Gautier <aga@zenexity.com>
2013-05-17 12:18:33 +0200

    Implement an implicit to convert a jsPath to mongo crud syntax

--- src/main/scala/play/modules/reactivemongo/json.scala
+++ src/main/scala/play/modules/reactivemongo/json.scala
@@ -236,3 +236,27 @@ object BSONFormats {
     orElse(BSONDocumentFormat.partialWrites).
     lift(bson).getOrElse(throw new RuntimeException(s"unhandled json value: $bson"))
 }
+
+object Writers {
+  implicit class JsPathMongo(val jp: JsPath) extends AnyVal {
+    def writemongo[A](implicit writer: Writes[A]): OWrites[A] = {
+      OWrites[A] { (o: A) =>
+        val newPath = jp.path.flatMap {
+          case e: KeyPathNode     => Some(e.key)
+          case e: RecursiveSearch => Some(s"$$.${e.key}")
+          case e: IdxPathNode     => Some(s"${e.idx}")
+        }.mkString(".")
+
+        val orig = writer.writes(o)
+        orig match {
+          case JsObject(e) =>
+            JsObject(e.flatMap {
+              case (k, v) => Seq(s"${newPath}.${k}" -> v)
+            })
+          case e: JsValue => JsObject(Seq(newPath -> e))
+        }
+      }
+    }
+  }
+}
+
