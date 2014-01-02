7ba81103b933d388919f1688eea9d0edd90ab80b
Stephane Godbillon <stephane@godbillon.fr>
2014-01-02 16:08:19 +0100

    update project def to 0.10.1
    Conflicts:
    	project/Play2-ReactiveMongo.scala

--- project/Play2-ReactiveMongo.scala
+++ project/Play2-ReactiveMongo.scala
@@ -2,7 +2,7 @@ import sbt._
 import sbt.Keys._
 
 object BuildSettings {
-  val buildVersion = "0.10.1-SNAPSHOT"
+  val buildVersion = "0.10.1"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.reactivemongo",
