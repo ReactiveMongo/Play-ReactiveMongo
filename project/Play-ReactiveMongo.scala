42f8416f18e71c0d0e0fdd37cdad405232878c0c
Duncan Crawford <duncan@tandemconsulting.eu>
2014-03-18 04:44:21 +0000

    Increment to fixed version to workaround publishing error

--- project/Play-ReactiveMongo.scala
+++ project/Play-ReactiveMongo.scala
@@ -6,7 +6,7 @@ object PlayReactiveMongoBuild extends Build {
   import uk.gov.hmrc.DefaultBuildSettings
 
   lazy val pluginName = "Play-ReactiveMongo"
-  val pluginVersion = "1.0.1-SNAPSHOT"
+  val pluginVersion = "1.0.1"
 
   lazy val pluginDependencies = Seq(
     "uk.gov.hmrc" %% "simple-reactivemongo" % "1.0.2" cross CrossVersion.binary,
