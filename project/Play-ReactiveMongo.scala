6a664825817b7ba922fb65ab6d59b967e8a979fa
Duncan Crawford <duncan@tandemconsulting.eu>
2014-06-03 14:47:33 +0100

    increment fixed version 2.3.0 & simple-reactivemongo to 1.3.0

--- project/Play-ReactiveMongo.scala
+++ project/Play-ReactiveMongo.scala
@@ -6,17 +6,16 @@ object PlayReactiveMongoBuild extends Build {
   import uk.gov.hmrc.DefaultBuildSettings
 
   lazy val pluginName = "Play-ReactiveMongo"
-  val pluginVersion = "2.2.0"
+  val pluginVersion = "2.3.0"
 
   lazy val pluginDependencies = Seq(
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "1.2.0" cross CrossVersion.binary,
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "1.2.0" % "test" cross CrossVersion.binary classifier "tests",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "1.3.0" cross CrossVersion.binary,
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "1.3.0" % "test" cross CrossVersion.binary classifier "tests",
 
     "com.typesafe.play" %% "play" % "[2.2.1,2.2.3]" % "provided" cross CrossVersion.binary,
     "com.typesafe.play" %% "play-test" % "[2.2.1,2.2.3]" % "test" cross CrossVersion.binary,
 
-    "org.scalatest" %% "scalatest" % "2.1.5" % "test" cross CrossVersion.binary,
-    "junit" % "junit" % "4.11" % "test" cross CrossVersion.Disabled,
+    "org.scalatest" %% "scalatest" % "2.1.7" % "test" cross CrossVersion.binary,
     "org.pegdown" % "pegdown" % "1.4.2" % "test" cross CrossVersion.Disabled
   )
 
