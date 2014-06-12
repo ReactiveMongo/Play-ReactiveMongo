edfe4a46f7654059cac683c03dc7ad6e12f1b412
Duncan Crawford <duncan@tandemconsulting.eu>
2014-06-12 17:01:33 +0100

    release 2.5.0

--- project/Play-ReactiveMongo.scala
+++ project/Play-ReactiveMongo.scala
@@ -6,7 +6,7 @@ object PlayReactiveMongoBuild extends Build {
   import uk.gov.hmrc.DefaultBuildSettings
 
   lazy val pluginName = "Play-ReactiveMongo"
-  val pluginVersion = "2.5.0-SNAPSHOT"
+  val pluginVersion = "2.5.0"
 
   lazy val pluginDependencies = Seq(
     "uk.gov.hmrc" %% "simple-reactivemongo" % "1.3.0" % "provided" cross CrossVersion.binary,
