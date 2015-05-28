75d2cefeee93dd1aaf2af4631500a13e444d22d1
Steve Smith <steve@alwaysagileconsulting.com>
2015-05-28 11:40:06 +0100

    MDTP-32 Add sbt-auto-build and sbt-git-versioning

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -1,19 +1,16 @@
 import sbt._
 import sbt.Keys._
+import uk.gov.hmrc.versioning.SbtGitVersioning
+import uk.gov.hmrc.{SbtAutoBuildPlugin}
 
 object HmrcBuild extends Build {
 
   import uk.gov.hmrc.DefaultBuildSettings
   import DefaultBuildSettings._
-  import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt}
-
-  val appVersion = "3.4.2-SNAPSHOT"
-
-  val simpleReactiveMongoVersion = "2.6.1"
 
   lazy val pluginDependencies = Seq(
-    "uk.gov.hmrc" %% "simple-reactivemongo" % simpleReactiveMongoVersion % "provided",
-    "uk.gov.hmrc" %% "simple-reactivemongo" % simpleReactiveMongoVersion % "test" classifier "tests",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "2.6.1" % "provided",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "2.6.1" % "test" classifier "tests",
 
     "com.typesafe.play" %% "play" % "2.3.8" % "provided",
     "com.typesafe.play" %% "play-test" % "2.3.8" % "test",
@@ -23,12 +20,9 @@ object HmrcBuild extends Build {
   )
 
   lazy val playReactiveMongo = Project("Play-ReactiveMongo", file("."))
-    .settings(version := appVersion)
-    .settings(scalaSettings : _*)
-    .settings(defaultSettings() : _*)
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
     .settings(
       targetJvm := "jvm-1.7",
-      shellPrompt := ShellPrompt(appVersion),
       libraryDependencies ++= pluginDependencies,
       resolvers := Seq(
         Resolver.bintrayRepo("hmrc", "releases"),
@@ -36,5 +30,4 @@ object HmrcBuild extends Build {
       ),
       crossScalaVersions := Seq("2.11.6")
     )
-    .settings(SbtBuildInfo(): _*)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,6 @@
-addSbtPlugin("uk.gov.hmrc" % "sbt-utils" % "2.5.0")
+resolvers += Resolver.url("hmrc-sbt-plugin-releases",
+  url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "0.5.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.4.0")
