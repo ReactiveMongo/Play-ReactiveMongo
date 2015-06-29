e673de2fb06d5f6f0fc64f3b9d94319fbfbd4a8a
cchantep <chantepie@altern.org>
2015-06-29 11:19:32 +0200

    Update the driver dependency

--- project/Play2-ReactiveMongo.scala
+++ project/Play2-ReactiveMongo.scala
@@ -2,7 +2,7 @@ import sbt._
 import sbt.Keys._
 
 object BuildSettings {
-  val buildVersion = "0.11.0.play24-M3"
+  val buildVersion = "0.11.0.play24"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.reactivemongo",
@@ -127,7 +127,7 @@ object Play2ReactiveMongoBuild extends Build {
         "Typesafe repository snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
       ),
       libraryDependencies ++= Seq(
-        "org.reactivemongo" %% "reactivemongo" % "0.11.0-M3" cross CrossVersion.binary,
+        "org.reactivemongo" %% "reactivemongo" % "0.11.0" cross CrossVersion.binary,
         "com.typesafe.play" %% "play" % "2.4.0" % "provided" cross CrossVersion.binary,
         "com.typesafe.play" %% "play-test" % "2.4.0" % "test" cross CrossVersion.binary,
         "org.specs2" % "specs2" % "2.3.12" % "test" cross CrossVersion.binary,
