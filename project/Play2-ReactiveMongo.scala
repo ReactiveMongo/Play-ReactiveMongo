fb99eac5917e8dc83a94c434761019123711832f
cchantep <chantepie@altern.org>
2016-06-04 19:18:33 +0200

    Update specs2 dependency

--- project/Play2-ReactiveMongo.scala
+++ project/Play2-ReactiveMongo.scala
@@ -116,7 +116,7 @@ object ShellPrompt {
 object Play2ReactiveMongoBuild extends Build {
   import BuildSettings._
 
-  val specsVersion = "3.6"
+  val specsVersion = "3.8.2"
   val specs2Dependencies = Seq(
     "specs2-core",
     "specs2-junit"
