9e18c7cf33932a4117fbdd704d5b591de7724570
cchantep <chantepie@altern.org>
2016-06-26 17:09:40 +0200

    Update to 0.11.14

--- project/Play2-ReactiveMongo.scala
+++ project/Play2-ReactiveMongo.scala
@@ -2,7 +2,7 @@ import sbt._
 import sbt.Keys._
 
 object BuildSettings {
-  val buildVersion = "0.11.13"
+  val buildVersion = "0.11.14"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.reactivemongo",
