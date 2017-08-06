1cec430d65d3155ea4e9c893f26a583ff08cc774
cchantep <chantepie@altern.org>
2017-08-06 22:01:53 +0200

    Fix compatibility with Play controller

---
+++ src/main/play-2.6/modules/reactivemongo/PlaySupport.scala
@@ -0,0 +1,7 @@
+package play.modules.reactivemongo
+
+import play.api.mvc.{ AbstractController => Ctrl }
+
+object PlaySupport {
+  type Controller = Ctrl
+}
---
+++ src/main/play-upto2.5/modules/reactivemongo/PlaySupport.scala
@@ -0,0 +1,7 @@
+package play.modules.reactivemongo
+
+import play.api.mvc.{ Controller => Ctrl }
+
+object PlaySupport {
+  type Controller = Ctrl
+}
