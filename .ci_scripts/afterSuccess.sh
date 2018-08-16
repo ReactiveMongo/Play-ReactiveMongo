f48e1bf1b276095cb07ce39f7492f243fe0d1b38
Cédric Chantepie <cchantep@users.noreply.github.com>
2018-08-17 00:23:06 +0200

    Refactoring post 0.16 (#263)

--- .ci_scripts/afterSuccess.sh
+++ .ci_scripts/afterSuccess.sh
@@ -1,18 +1,31 @@
-#! /bin/sh
+#! /usr/bin/env bash
+
+set -e
 
 SCRIPT_DIR=`dirname $0`
 
-# TRAVIS_BRANCH
-if [ ! "x$TRAVIS_TAG" = "x" -o "$SONATYPE_USER" = "" -o "$SONATYPE_PASS" = "" ]; then
-    echo "skip the snapshot publication"
+if [[ "$CI_CATEGORY" != "UNIT_TESTS" ]]; then
+  killall -9 mongod 2>&1 || true
+fi
+
+rm -rf "$HOME/.ivy2/cache/org.reactivemongo/"
+
+if [[ "$CI_BRANCH" != "master" || "x$PUBLISHABLE" != "xyes" || \
+      "x$SONATYPE_USER" = "x" || "x$SONATYPE_PASS" = "x" ]]; then
+
+    U=`echo "$SONATYPE_USER" | sed -e 's/./X/g'`
+    P=`echo "$SONATYPE_PASS" | sed -e 's/./X/g'`
+
+    echo -e -n "\nINFO: Skip the snapshot publication (${CI_BRANCH}, $PUBLISHABLE ${U}:${P})\n"
+
     exit 0
 fi
 
 cd "$SCRIPT_DIR/.."
 
 export PUBLISH_REPO_NAME="Sonatype Nexus Repository Manager"
+export PUBLISH_REPO_URL="https://oss.sonatype.org/content/repositories/snapshots"
 export PUBLISH_REPO_ID="oss.sonatype.org"
-export PUBLISH_REPO_URL=https://oss.sonatype.org/content/repositories/snapshots/
 export PUBLISH_USER="$SONATYPE_USER"
 export PUBLISH_PASS="$SONATYPE_PASS"
 
