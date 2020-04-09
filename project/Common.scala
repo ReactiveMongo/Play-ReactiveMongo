6ad25afa5baf31c238da4ba9a2fa7dad79312809
Cédric Chantepie <cchantep@users.noreply.github.com>
2020-04-09 09:06:41 +0200

    PathBindables for Bison API

--- project/Common.scala
+++ project/Common.scala
@@ -18,7 +18,7 @@ object Common extends AutoPlugin {
     scalaVersion := "2.12.10",
     useShaded := sys.env.get("REACTIVEMONGO_SHADED").fold(true)(_.toBoolean),
     driverVersion := {
-      val v = (version in ThisBuild).value
+      val v = "0.20.3" // TODO: (version in ThisBuild).value
       val suffix = {
         if (useShaded.value) "" // default ~> no suffix
         else "-noshaded"
@@ -35,7 +35,7 @@ object Common extends AutoPlugin {
             s"${major}-${suffix}"
 
           case vs @ _ =>
-            ((vs.init :+ "play27") ++ vs.lastOption.toList).mkString("-")
+            ((vs.init :+ suffix) ++ vs.lastOption.toList).mkString("-")
         }
 
         case _ => ver
