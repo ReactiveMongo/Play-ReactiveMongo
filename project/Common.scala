4851a1fc6f874eac1e2e8b4259fb01418cb83eb8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-12-01 21:10:39 +0100

    Update play, play-test to 2.8.11 (#424)

--- project/Common.scala
+++ project/Common.scala
@@ -74,7 +74,7 @@ object Common extends AutoPlugin {
     Publish.mimaSettings ++ Release.settings)
 
   lazy val playLower = "2.5.0"
-  lazy val playUpper = "2.8.10"
+  lazy val playUpper = "2.8.11"
   lazy val playVer = Def.setting[String] {
     sys.env.get("PLAY_VERSION").getOrElse {
       if (scalaBinaryVersion.value == "2.11") playLower
