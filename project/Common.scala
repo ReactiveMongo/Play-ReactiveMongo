49f3cc50a8e380512c9ff9d76d5641a6610b2ef4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-10-18 09:40:11 +0200

    Update play, play-test to 2.8.18 (#481)

--- project/Common.scala
+++ project/Common.scala
@@ -80,7 +80,7 @@ object Common extends AutoPlugin {
   ) ++ Publish.settings ++ Publish.mimaSettings ++ Release.settings
 
   lazy val playLower = "2.5.0"
-  lazy val playUpper = "2.8.16"
+  lazy val playUpper = "2.8.18"
 
   lazy val playVer = Def.setting[String] {
     sys.env.get("PLAY_VERSION").getOrElse {
